e3ef2f53540f452434551fec025b32f4eb3689b1
= <=>
2024-08-17 14:10:23 +0200

    Separate logic from SFC

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,11 @@
   <hr />
   <MyCounter :value="5" text="Hola mundo" />
   <MyCounterScript :value="3" />
+  <MyCounterScript2 :value="7" />
 </template>
 
 <script lang="ts" setup>
 import MyCounter from './components/MyCounter.vue';
 import MyCounterScript from './components/MyCounterScript.vue';
+import MyCounterScript2 from './components/my-counter-script/MyCounterScript2.vue';
 </script>
