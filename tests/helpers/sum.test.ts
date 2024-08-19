52f05dd23112396335d8f3b926095e821b58f735
= <=>
2024-08-19 18:11:29 +0200

    Testing: addArray test summatory

--- tests/helpers/sum.test.ts
+++ tests/helpers/sum.test.ts
@@ -1,14 +1,37 @@
 import { expect, test } from 'vitest';
-import { sum } from '../../src/helpers/sum.helper';
+import { describe } from 'node:test';
+import { sum, addArray } from '../../src/helpers/sum.helper';
 
-test('adds 1 + 2 to equal 3', () => {
-  // Preparación
-  const a = 1;
-  const b = 2;
+describe('add functions', () => {
+  test('adds 1 + 2 to equal 3', () => {
+    // Preparación
+    const a = 1;
+    const b = 2;
 
-  // Estímulo
-  const result = sum(a, b);
+    // Estímulo
+    const result = sum(a, b);
 
-  // Comportamiento esperado
-  expect(result).toBe(a + b);
+    // Comportamiento esperado
+    expect(result).toBe(a + b);
+  });
+});
+
+describe('addAray function', () => {
+  test('shuold return 0 if the array is empty'),
+    () => {
+      const numberArray = [];
+
+      const result = addArray(numberArray);
+
+      expect(result).toBe(0);
+    };
+
+  test('shuold return the proper value of the addArray function'),
+    () => {
+      const numberArray = [5, 10, 25];
+
+      const result = addArray(numberArray);
+
+      expect(result).toBe(40);
+    };
 });
